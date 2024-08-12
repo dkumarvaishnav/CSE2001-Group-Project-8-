40e965f668638f75436e3d5e23eaf69aa504a913
Lokesh Bagade <lbagade6@gmail.com>
2024-08-12 23:07:58 +0530

    Created New File For Answer 1

---
+++ Activity 1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Date {
+private:
+    int day;
+    int month;
+    int year;
+
+    // Helper function to check if a year is a leap year
+    bool isLeapYear() const {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    // Helper function to get the number of days in a month
+    int daysInMonth() const {
+        int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month == 2 && isLeapYear()) {
+            return 29;
+        } else {
+            return days[month - 1];
+        }
+    }
