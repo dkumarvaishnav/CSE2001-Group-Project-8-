eef864d98415077808c877855068148bc06fa285
Adarsh-0703 <adarshaditya.mishra@gmail.com>
2024-08-12 23:45:06 +0530

    Update main1.cpp

--- Activity 4/main1.cpp
+++ Activity 4/main1.cpp
@@ -14,3 +14,15 @@ T findLargest(T a, T b, T c) {
 
     return largest;
 };
+
+int main() {
+    int x = 10, y = 25, z = 5;
+    double a = 3.14, b = 2.71, c = 1.62;
+    string str1 = "apple", str2 = "banana", str3 = "cherry";
+
+    cout << "Largest integer: " << findLargest(x, y, z) << endl;
+    cout << "Largest double: " << findLargest(a, b, c) << endl;
+    cout << "Largest string: " << findLargest(str1, str2, str3) << endl;
+
+    return 0;
+}
