3c977c781286eeb3a49f119c0fb16e06fb455050
KRITIKA233CODER <kritikaniranjandangi@gmail.com>
2024-08-12 23:41:24 +0530

    Update main1.cpp

---
+++ Activity 4/main1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+template <typename T>
+T findLargest(T a, T b, T c) {
+    T largest = a;
+
+    if (b > largest) {
+        largest = b;
+    }
+    if (c > largest) {
+        largest = c;
+    }
+
+    return largest;
+};
